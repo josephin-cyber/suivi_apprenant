6e7f1cd48b81966a630a2713cad86b76013db550
tom hikari <tomprescott1565@gmail.com>
2021-02-28 12:01:02 +0100

    login_jwt

--- controlers/login_controller.js
+++ controlers/login_controller.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 const accessTokenSecret=require("../config/jwt");
 
 const get = (connection, req, res) => {
-    connection.query(`select * from utiisateur WHERE email_utiisateur='${req.fields.email}' AND pwd_utilisateur=SHA1('${req.fields.pwd}')`,
+    connection.query(`select * from  utilisateur WHERE email_utilisateur='${req.fields.email}' AND pwd_utilisateur=SHA1('${req.fields.pwd}')`,
       (error, data) => {
         if (error) return res.status(401).json({ error });
-        const accessToken = jwt.sign({data}, accessTokenSecret);
-        return res.json({ accessToken }).status(200);
+        const accessToken = jwt.sign({data}, accessTokenSecret. accessTokenSecret);
+        return res.json({ accessToken, data}).status(200);
       }
     );
   };
